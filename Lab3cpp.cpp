5332e89f246810030423e820c0f40059effa3ce3
Givikap120 <89256026+Givikap120@users.noreply.github.com>
2021-09-22 19:30:01 +0300

    Add files via upload

---
+++ Lab3cpp.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    double awnser = 1, awnser_prev = 0, input, chiselnik = 1;
+    long znamennik = 1, factor = 0;
+    cin >> input;
+    while (long(awnser * 100000) != long(awnser_prev * 100000)) {
+        awnser_prev = awnser;
+        factor++;
+        chiselnik *= input;
+        znamennik *= factor;
+        awnser += chiselnik / znamennik;
+    }
+    cout << float(long(awnser * 100000)) / 100000 << endl;
+    system("pause");
+}
+
