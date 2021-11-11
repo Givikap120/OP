ba6633fc9a87de4a4c6b9f3d7a4cde9e593908bc
Givikap120 <89256026+Givikap120@users.noreply.github.com>
2021-11-11 11:46:08 +0200

    Add files via upload

---
+++ Lab5cpp.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+long round_up(double number) {
+    if (long(number) != number) number = long(number) + 1;
+    return long(number);
+}
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    int n;
+    long n_fact = 1, a2;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        n_fact *= i;
+    }
+    a2 = round_up(pow(n_fact, 1.0 / 3));
+
+    bool is_suitable = true;
+    if (n_fact % (a2 + 1) != 0) is_suitable = false;
+    else if (n_fact % (a2) != 0) is_suitable = false;
+    else if (n_fact % (a2 - 1) != 0) is_suitable = false;
+
+    if (is_suitable == true) cout << "Так, можна: " << a2 - 1 << " * " << a2 << " * " << a2 + 1 << " = " << n_fact;
+    else cout << "Нi, не можна";
+}
+
---
+++ Lab6cpp.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+using namespace std;
+
+double max(double a, double b) {
+    if (a >= b) return a;
+    else return b;
+}
+
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << (max(a, a + b) + max(a, b + c)) / (1 + max(a + b * c, 1.15));
+}
+
